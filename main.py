ee8a3b78c52d0548b591b5287171b683bc041851
Mateusz Kozłowski <ct14kozlowskim@gmail.com>
2020-09-06 00:33:39 +0200

    Modified main.py, added quit option

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Assumptions:
 - snake can eat apples and save it as a score
 - snake grow when eat apple
 """
-import time
+import time,os,keyboard
 from board import Board
 
 class Game:
@@ -13,12 +13,22 @@ class Game:
     def __init__(self):
         self.game_on = True
         self.board = Board()
-
-    def run_game(self,game_on):
-        pass
+    
+    def read_keyboard(self,key):
+        if key.name == "esc":
+            self.game_on = False
+        keyboard.unhook_all()
+            
+    def update_screen(self):
+        print("-------------PRESS 'esc' TO QUIT-------------")
+        self.board.show_board()
+        keyboard.on_press(self.read_keyboard)
+        time.sleep(0.5)
+        os.system('clear')
 
 
 if __name__ == "__main__":
-
     snk_game = Game()
-    snk_game.board.show_board()
+    while(snk_game.game_on):
+        snk_game.update_screen()
+    print("Thanks for playing")
