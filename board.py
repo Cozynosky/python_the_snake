68bac157083a8c59dcc5eb55e5d8b70fe12e4f08
thinkofher <piaskupiasku@gmail.com>
2020-09-24 22:17:35 +0200

    Applied black formatting.

--- board.py
+++ board.py
@@ -5,19 +5,19 @@ import time
 from snake import Snake
 from apple import Apple
 
-class Board:
 
-    def __init__(self,settings):
+class Board:
+    def __init__(self, settings):
         self.width = settings.board_width
         self.height = settings.board_height
         self.board = self.make_board()
         self.snake = Snake(settings)
-        self.apple = Apple(settings,self.snake)
+        self.apple = Apple(settings, self.snake)
         self.score = 0
         self.playtime_started = time.time()
         self.playtime_ended = 0
         self.settings = settings
-    
+
     def make_board(self):
         board = [[" " for y in range(self.width)] for x in range(self.height)]
         for i in range(self.width):
@@ -35,16 +35,17 @@ class Board:
     def show_board(self):
         self.playtime_ended = int(time.time() - self.playtime_started)
         minutes = self.playtime_ended // 60
-        seconds = self.playtime_ended - (minutes *60)
-        print(f"""+-------------------------------------------+
-         SCORE:{self.score}   TIME PLAYED: {minutes}:{seconds}""")
+        seconds = self.playtime_ended - (minutes * 60)
+        print(
+            f"""+-------------------------------------------+
+         SCORE:{self.score}   TIME PLAYED: {minutes}:{seconds}"""
+        )
         for i in range(self.height):
             for j in range(self.width):
                 print(self.board[i][j], end="")
             print("")
         print("""             PRESS 'esc' TO QUIT             """)
 
-    
     def update_board(self):
         self.board = self.make_board()
         self.snake.move()
@@ -55,9 +56,6 @@ class Board:
         self.board[self.snake.body[0][0]][self.snake.body[0][1]] = "O"
         if self.snake.body[0] == self.apple.apple_coords:
             self.score += 1
-            self.settings.game_speed *= 0.97 
+            self.settings.game_speed *= 0.97
             self.snake.grow = True
             self.apple.spawn_apple(self.snake)
-        
-        
-            
--- snake.py
+++ snake.py
@@ -2,45 +2,47 @@
 Snake class with his body length, positions and some methods
 """
 
-class Snake:
 
-    def __init__(self,settings):
+class Snake:
+    def __init__(self, settings):
         self.settings = settings
         self.x_step = 0
         self.y_step = -1
-        self.body = [(settings.board_height//2,settings.board_width//2),
-        ((settings.board_height//2)+1,settings.board_width//2),
-        ((settings.board_height//2)+2,settings.board_width//2)]
+        self.body = [
+            (settings.board_height // 2, settings.board_width // 2),
+            ((settings.board_height // 2) + 1, settings.board_width // 2),
+            ((settings.board_height // 2) + 2, settings.board_width // 2),
+        ]
         self.grow = False
-    
+
     def go_up(self):
-        if len(self.body)>1 and self.y_step == 1:
+        if len(self.body) > 1 and self.y_step == 1:
             pass
-        else: 
+        else:
             self.y_step = -1
             self.x_step = 0
 
     def go_down(self):
-        if len(self.body)>1 and self.y_step == -1:
+        if len(self.body) > 1 and self.y_step == -1:
             pass
-        else: 
+        else:
             self.y_step = 1
             self.x_step = 0
 
     def go_left(self):
-        if len(self.body)>1 and self.x_step == 2:
+        if len(self.body) > 1 and self.x_step == 2:
             pass
-        else: 
+        else:
             self.y_step = 0
             self.x_step = -2
 
     def go_right(self):
-        if len(self.body)>1 and self.x_step == -2:
+        if len(self.body) > 1 and self.x_step == -2:
             pass
-        else: 
+        else:
             self.y_step = 0
             self.x_step = 2
-    
+
     def move(self):
         body_length = len(self.body)
         if self.grow:
@@ -48,26 +50,23 @@ class Snake:
             self.grow = False
         if len(self.body) > 1:
             body_copy = self.body[:]
-            for i in range(1,body_length):
-                self.body[i] = body_copy[i-1]
-        self.body[0] = (self.body[0][0]+self.y_step, self.body[0][1]+self.x_step)
+            for i in range(1, body_length):
+                self.body[i] = body_copy[i - 1]
+        self.body[0] = (self.body[0][0] + self.y_step, self.body[0][1] + self.x_step)
 
     def wall_detect(self):
         for i in range(len(self.body)):
             if self.body[i][0] <= 0:
-                self.body[i] = (self.settings.board_height - 2,self.body[i][1])
+                self.body[i] = (self.settings.board_height - 2, self.body[i][1])
             if self.body[i][0] >= self.settings.board_height - 1:
-                self.body[i] = (1,self.body[i][1])
+                self.body[i] = (1, self.body[i][1])
             if self.body[i][1] <= 0:
-                self.body[i] = (self.body[i][0],self.settings.board_width-3)
-            if self.body[i][1] >= self.settings.board_width-1:
-                self.body[i] = (self.body[i][0],2)
+                self.body[i] = (self.body[i][0], self.settings.board_width - 3)
+            if self.body[i][1] >= self.settings.board_width - 1:
+                self.body[i] = (self.body[i][0], 2)
 
     def self_eat_detect(self):
-        if len(self.body)>3 and self.body[0] in self.body[1:]:
+        if len(self.body) > 3 and self.body[0] in self.body[1:]:
             return True
         else:
             return False
-    
-
-    
